b12a45b16dfcb4dfad32a6ee9ed99436ce98a1d1
Greg Brown <gregory.w.brown@gmail.com>
2023-04-21 17:15:22 -0500

    Initialize repository

---
+++ lib/browngw_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "browngw_palindrome/version"
+
+module BrowngwPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/browngw_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BrowngwPalindrome
+  VERSION = "0.1.0"
+end
